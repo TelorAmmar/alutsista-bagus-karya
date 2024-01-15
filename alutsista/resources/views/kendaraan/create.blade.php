5ae195f93ea034034d06c401e2e6a16cb77dbd51
Hafidz Ammar <21102074@ittelkom-pwt.ac.id>
2024-01-15 11:25:45 +0700

    fix href, input

--- alutsista/resources/views/kendaraan/create.blade.php
+++ alutsista/resources/views/kendaraan/create.blade.php
@@ -26,7 +26,7 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active" href="/kendaraan">Back</a>
+                        <a class="nav-link active" href="{{ route('kendaraan.index') }}">Back</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link active" aria-current="page" href="/">Home</a>
@@ -38,106 +38,115 @@
 <!-- navbar end -->
 <!-- form Section start -->
 <form action="{{ route('kendaraan.store') }}" method="POST" class="form" enctype="multipart/form-data">
-  @csrf
-                    <div class="nama-alutsista">
-                        <label for="nama">Nama Senjata</label>
-                        <input type="text"
-                        class="form-control @error('nama') is-invalid @enderror"
-                        id="nama" name="nama" value="{{ old('nama') }}">
-                        @error('nama')
-                            <div class="text-danger">{{ $message }}</div>
-                        @enderror
-                    </div>
-                    <div class="nomor-seri">
-                        <label for="no_seri">Nomor Seri</label>
-                        <input type="text"
-                        class="form-control @error('no_seri') is-invalid @enderror"
-                        id="no_seri" name="no_seri" value="{{ old('no_seri') }}">
-                        @error('no_seri')
-                            <div class="text-danger">{{ $message }}</div>
-                        @enderror
-                    </div>
-                    <div class="kapasitas">
-                        <label for="kapasitas">Kapasitas</label>
-                        <input type="text"
-                        class="form-control @error('kapasitas') is-invalid @enderror"
-                        id="kapasitas" name="kapasitas" value="{{ old('kapasitas') }}">
-                        @error('kapasitas')
-                            <div class="text-danger">{{ $message }}</div>
-                        @enderror
-                    </div>
-                    <div class="ukuran">
-                        <label for="ukuran">Ukuran</label>
-                        <input type="text"
-                        class="form-control @error('ukuran') is-invalid @enderror"
-                        id="ukuran" name="ukuran" value="{{ old('ukuran') }}">
-                        @error('ukuran')
-                            <div class="text-danger">{{ $message }}</div>
-                        @enderror
-                    </div>
-                    <div class="berat">
-                        <label for="berat">Berat</label>
-                        <input type="text"
-                        class="form-control @error('berat') is-invalid @enderror"
-                        id="berat" name="berat" value="{{ old('berat') }}">
-                        @error('berat')
-                            <div class="text-danger">{{ $message }}</div>
-                        @enderror
-                    </div>
-                    <div class="daya-tembak">
-                        <label for="daya_tembak">Daya Tembak</label>
-                        <input type="text"
-                        class="form-control @error('daya_tembak') is-invalid @enderror"
-                        id="daya_tembak" name="daya_tembak" value="{{ old('daya_tembak') }}">
-                        @error('daya_tembak')
-                            <div class="text-danger">{{ $message }}</div>
-                        @enderror
-                    </div>
-                    <div class="kecepatan">
-                        <label for="kecepatan">Kecepatan</label>
-                        <input type="text"
-                        class="form-control @error('kecepatan') is-invalid @enderror"
-                        id="kecepatan" name="kecepatan" value="{{ old('kecepatan') }}">
-                        @error('kecepatan')
-                            <div class="text-danger">{{ $message }}</div>
-                        @enderror
-                    </div>
-                    <div class="status-kondisi">
-                        <label for="status">Status</label>
-                        <input type="text"
-                        class="form-control @error('status') is-invalid @enderror"
-                        id="status" name="status" value="{{ old('status') }}">
-                        @error('status')
-                            <div class="text-danger">{{ $message }}</div>
-                        @enderror
-                    </div>
-                    <div class="pemilik-lokasi">
-                        <label for="pemilik">Pemilik</label>
-                        <input type="text"
-                        class="form-control @error('pemilik') is-invalid @enderror"
-                        id="pemilik" name="pemilik" value="{{ old('pemilik') }}">
-                        @error('pemilik')
-                            <div class="text-danger">{{ $message }}</div>
-                        @enderror
-                    </div>
-                    <div class="sejarah-pemeliharaan">
-                        <label for="sejarah">Sejarah</label>
-                        <textarea class="form-control" id="sejarah"
-                        name="sejarah">{{ old('sejarah') }}</textarea>
-                    </div>
-                    <div class="pemakaian-riwayat-operasi">
-                        <label for="pemakaian">Pemakaian dan Riwayat Oprasi</label>
-                        <textarea class="form-control" id="pemakaian"
-                        name="pemakaian">{{ old('pemakaian') }}</textarea>
-                    </div>
-                    <div class="tambah-gambar">
-                        <label for="image">Gambar</label>
-                        <input type="file" class="form-control-file" id="image" name="image">
-                        @error('image')
-                        <div class="text-danger">{{ $message }}</div>
-                        @enderror
-                    </div>
-                    <button type="submit" class="btn btn-danger">Submit</button>
+    @csrf
+    <div class="form-group">
+        <label for="nama">Nama Kendaraan</label>
+        <input type="text"
+        class="form-control @error('nama') is-invalid @enderror"
+        id="nama" name="nama" value="{{ old('nama') }}">
+        @error('nama')
+            <div class="text-danger">{{ $message }}</div>
+        @enderror
+    </div>
+    <div class="form-group">
+        <label for="no_seri">Nomor Seri</label>
+        <input type="text"
+        class="form-control @error('no_seri') is-invalid @enderror"
+        id="no_seri" name="no_seri" value="{{ old('no_seri') }}">
+        @error('no_seri')
+            <div class="text-danger">{{ $message }}</div>
+        @enderror
+    </div>
+    <div class="form-group">
+        <label for="tipe">Tipe</label>
+        <input type="text"
+        class="form-control @error('tipe') is-invalid @enderror"
+        id="tipe" name="tipe" value="{{ old('tipe') }}">
+        @error('tipe')
+            <div class="text-danger">{{ $message }}</div>
+        @enderror
+    </div>
+    <div class="form-group">
+        <label for="kapasitas">Kapasitas</label>
+        <input type="text"
+        class="form-control @error('kapasitas') is-invalid @enderror"
+        id="kapasitas" name="kapasitas" value="{{ old('kapasitas') }}">
+        @error('kapasitas')
+            <div class="text-danger">{{ $message }}</div>
+        @enderror
+    </div>
+    <div class="form-group">
+        <label for="ukuran">Ukuran</label>
+        <input type="text"
+        class="form-control @error('ukuran') is-invalid @enderror"
+        id="ukuran" name="ukuran" value="{{ old('ukuran') }}">
+        @error('ukuran')
+            <div class="text-danger">{{ $message }}</div>
+        @enderror
+    </div>
+    <div class="form-group">
+        <label for="berat">Berat</label>
+        <input type="text"
+        class="form-control @error('berat') is-invalid @enderror"
+        id="berat" name="berat" value="{{ old('berat') }}">
+        @error('berat')
+            <div class="text-danger">{{ $message }}</div>
+        @enderror
+    </div>
+    <div class="form-group">
+        <label for="kecepatan">Kecepatan</label>
+        <input type="text"
+        class="form-control @error('kecepatan') is-invalid @enderror"
+        id="kecepatan" name="kecepatan" value="{{ old('kecepatan') }}">
+        @error('kecepatan')
+            <div class="text-danger">{{ $message }}</div>
+        @enderror
+    </div>
+    <div class="form-group">
+        <label for="proteksi">Proteksi</label>
+        <input type="text"
+        class="form-control @error('proteksi') is-invalid @enderror"
+        id="proteksi" name="proteksi" value="{{ old('proteksi') }}">
+        @error('proteksi')
+            <div class="text-danger">{{ $message }}</div>
+        @enderror
+    </div>
+    <div class="form-group">
+        <label for="status">Status</label>
+        <input type="text"
+        class="form-control @error('status') is-invalid @enderror"
+        id="status" name="status" value="{{ old('status') }}">
+        @error('status')
+            <div class="text-danger">{{ $message }}</div>
+        @enderror
+    </div>
+    <div class="form-group">
+        <label for="pemilik">Pemilik</label>
+        <input type="text"
+        class="form-control @error('pemilik') is-invalid @enderror"
+        id="pemilik" name="pemilik" value="{{ old('pemilik') }}">
+        @error('pemilik')
+            <div class="text-danger">{{ $message }}</div>
+        @enderror
+    </div>
+    <div class="form-group">
+        <label for="sejarah">Sejarah</label>
+        <textarea class="form-control" id="sejarah" rows="3"
+        name="sejarah">{{ old('sejarah') }}</textarea>
+    </div>
+    <div class="form-group">
+        <label for="pemakaian">Pemakaian</label>
+        <textarea class="form-control" id="pemakaian" rows="3"
+        name="pemakaian">{{ old('pemakaian') }}</textarea>
+    </div>
+    <div class="form-group">
+        <label for="image">Gambar</label>
+        <input type="file" class="form-control-file" id="image" name="image">
+        @error('image')
+        <div class="text-danger">{{ $message }}</div>
+        @enderror
+    </div>
+    <button type="submit" class="btn btn-primary mb-2">Submit</button>
 </form>
 <!-- form Section end -->
 
--- alutsista/resources/views/kendaraan/index.blade.php
+++ alutsista/resources/views/kendaraan/index.blade.php
@@ -19,7 +19,7 @@
 <!-- navbar -->
     <nav class="navbar navbar-expand-lg navbar-dark py-3 fixed-top">
         <div class="container">
-            <a class="navbar-logo" href="#">Alutsista<span>Indonesia</span>.</a>
+            <a class="navbar-logo" href="{{ route('index') }}">Alutsista<span>Indonesia</span>.</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
               </button>
@@ -27,13 +27,13 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="/">Home</a>
+                        <a class="nav-link active" aria-current="page" href="{{ route('index') }}">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link active" href="/kendaraan/create">Tambah</a>
+                        <a class="nav-link active" href="{{ route('kendaraan.create') }}">Tambah</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link active" href="/pinjam">Peminjaman</a>
+                        <a class="nav-link active" href="{{ route('pinjam.index') }}">Peminjaman</a>
                     </li>
                     </ul>
                 </div>
