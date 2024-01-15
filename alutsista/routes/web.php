e7741c00d1235bdbbc65cd1b3aca750b2c63e7d0
Hafidz Ammar <21102074@ittelkom-pwt.ac.id>
2024-01-15 13:09:06 +0700

    routing

--- alutsista/routes/web.php
+++ alutsista/routes/web.php
@@ -25,20 +25,20 @@ Route::get('/', [Dasboard::class,'index'])->name('index');
 // Senjata
 Route::get('/senjata/create',[SenjataController::class,'create'])->name('senjata.create')->middleware('login_auth');
 Route::post('/senjata',[SenjataController::class,'store'])->name('senjata.store')->middleware('login_auth');
-Route::get('/senjata',[SenjataController::class,'index'])->name('senjata.index')->middleware('login_auth');
-Route::get('/senjata/{senjata}', [SenjataController::class,'show'])->name('senjata.show')->middleware('login_auth');
+Route::get('/senjata',[SenjataController::class,'index'])->name('senjata.index');
+Route::get('/senjata/{senjata}', [SenjataController::class,'show'])->name('senjata.show');
 Route::delete('/senjata/{senjata}',[SenjataController::class,'destroy'])->name('senjata.destroy')->middleware('login_auth');
 // Kendaraan
 Route::get('/kendaraan/create',[KendaraanController::class,'create'])->name('kendaraan.create')->middleware('login_auth');
 Route::post('/kendaraan',[KendaraanController::class,'store'])->name('kendaraan.store')->middleware('login_auth');
-Route::get('/kendaraan',[KendaraanController::class,'index'])->name('kendaraan.index')->middleware('login_auth');
-Route::get('/kendaraan/{kendaraan}', [KendaraanController::class,'show'])->name('kendaraan.show')->middleware('login_auth');
+Route::get('/kendaraan',[KendaraanController::class,'index'])->name('kendaraan.index');
+Route::get('/kendaraan/{kendaraan}', [KendaraanController::class,'show'])->name('kendaraan.show');
 Route::delete('/kendaraan/{kendaraan}',[KendaraanController::class,'destroy'])->name('kendaraan.destroy')->middleware('login_auth');
 // Pinjam
 Route::get('/pinjam/create',[PinjamController::class,'create'])->name('pinjam.create')->middleware('login_auth');
 Route::post('/pinjam',[PinjamController::class,'store'])->name('pinjam.store')->middleware('login_auth');
-Route::get('/pinjam',[PinjamController::class,'index'])->name('pinjam.index')->middleware('login_auth');
-Route::get('/pinjam/{pinjam}', [PinjamController::class,'show'])->name('pinjam.show')->middleware('login_auth');
+Route::get('/pinjam',[PinjamController::class,'index'])->name('pinjam.index');
+Route::get('/pinjam/{pinjam}', [PinjamController::class,'show'])->name('pinjam.show');
 Route::delete('/pinjam/{pinjam}',[PinjamController::class,'destroy'])->name('pinjam.destroy')->middleware('login_auth');
 // Login
 Route::get('/login', [AdminController::class,'index'])->name('login.index');
